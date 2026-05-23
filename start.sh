cb2f1955f2d453b3510b9caab90f501e7c61602e
cksac <cs.cksac@gmail.com>
2026-05-23 17:35:26 +0800

    Fix context size in start script for ds4-server

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 ./ds4-server -m ./gguf/DeepSeek-V4-Flash-IQ2XXS-w2Q2K-AProjQ8-SExpQ8-OutQ8-chat-v2-imatrix.gguf \
 --mtp ./gguf/DeepSeek-V4-Flash-MTP-Q4K-Q8_0-F32.gguf \
 --mtp-draft 3 \
---ctx 10000000 \
+--ctx 1000000 \
 --threads 16 \
 --metal \
 --kv-disk-dir ./kv_cache \
